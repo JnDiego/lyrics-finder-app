6d51e69c39c632d66ce7def8f0fff33c12692909
Juan Diego <diego_suarez@outlook.com>
2020-06-11 14:00:30 -0500

    Updates

---
+++ precache-manifest.ef6b30c0eaff655d563c8eb3bb150e9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0effb75abceba3238a84547c5e660360",
+    "url": "/lyrics-finder-app/index.html"
+  },
+  {
+    "revision": "b2aceff15fd328876040",
+    "url": "/lyrics-finder-app/static/css/main.f71191c7.chunk.css"
+  },
+  {
+    "revision": "23a0018344666bb187ff",
+    "url": "/lyrics-finder-app/static/js/2.0ef361c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lyrics-finder-app/static/js/2.0ef361c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2aceff15fd328876040",
+    "url": "/lyrics-finder-app/static/js/main.a634fda4.chunk.js"
+  },
+  {
+    "revision": "c4dba3c7833ccc252304",
+    "url": "/lyrics-finder-app/static/js/runtime-main.f4908613.js"
+  }
+]);
